5eb5123fae510f34213bf6c01954f1dc6333f68c
Raghav Pandit <panditraghav18@gmail.com>
2025-01-03 14:03:43 +0530

    Added two textures to the square

--- resources/shaders/shader.fs
+++ resources/shaders/shader.fs
@@ -1,7 +1,13 @@
 #version 330 core
 out vec4 fragColor;
+
 in vec3 vertexColor;
+in vec2 texCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
 void main()
 {
-  fragColor = vec4(vertexColor, 1.0f);
+  fragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2f);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "shader.hpp"
+#include "stb_image.h"
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -48,14 +49,16 @@ int main() {
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
   float vertices[] = {
-    // Vertices         // Colors
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
-     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
-     0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
+    // positions          // colors           // texture coords
+    0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+    0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
   };
 
   unsigned int indices[] = {  // note that we start from 0!
     0, 1, 2,   // first triangle
+    2, 3, 0,   // second triangle
   };
 
   unsigned int VBO; // Vertex Buffer Object, array buffer to store data on GPU
@@ -89,11 +92,17 @@ int main() {
    * arg4: Boolean specify data should be normalized or not
    * arg5: Strides, tells space between consecutive vertex attributes.
    */
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  // Vertices
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  // Colors
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+
+  // Texture Coordinates
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
   // Unbind buffer
   // note that this is allowed, the call to glVertexAttribPointer registered VBO as the
@@ -104,15 +113,67 @@ int main() {
   // buffer object IS stored in the VAO; keep the EBO bound.
   // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+  // -----Texture----
+  unsigned int texture1;
+  glGenTextures(1, &texture1);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  // set the texture wrapping/filtering options (on the currently bound texture object)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+
+  int width, height, nrChannels;
+  unsigned char* data = stbi_load("resources/textures/container.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else {
+    std::cerr << "Failed to load texture" << std::endl;
+  }
+  // Free the image after generating texture and minimap
+  stbi_image_free(data);
+
+  // Can unbind textures
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  unsigned int texture2;
+  glGenTextures(1, &texture2);
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  stbi_set_flip_vertically_on_load(true);
+
+  int width2, height2, nrChannels2;
+  unsigned char* texture2Image = stbi_load("resources/textures/awesomeface.png", &width2, &height2, &nrChannels2, 0);
+  if (texture2Image) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture2Image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else {
+    std::cerr << "Failed to load texture" << std::endl;
+  }
+  glBindTexture(GL_TEXTURE_2D,0);
+  // Free the image after generating texture and minimap
+  stbi_image_free(texture2Image);
+
   // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO,
   // but this rarely happens.
   // Modifying other VAOs requires a call to glBindVertexArray anyways so we generally
   // don't unbind VAOs (nor VBOs) when it's not directly necessary.
   glBindVertexArray(0);
 
-
   // uncomment this call to draw in wireframe polygons.
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+  ourShader.use();
+  ourShader.setInt(std::string("texture1"), 0);
+  ourShader.setInt(std::string("texture2"), 1);
 
   while (!glfwWindowShouldClose(window)) {
     // input
@@ -121,14 +182,20 @@ int main() {
 
     // render
     // ------
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClearColor(0.0f, 0.15f, 0.2f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
     ourShader.use();
 
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D,texture1);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D,texture2);
+
     glBindVertexArray(VAO);
     /*glDrawArrays(GL_TRIANGLES, 0, 3);*/
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 7, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     /*glBindVertexArray(sVAO);*/
